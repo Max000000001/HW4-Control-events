a76021cfaac2a207649bffb711106bc06ed912e8
Max <issitt.001@gmail.com>
2023-03-30 15:45:29 +0700

    release v1

---
+++ h04/src/classes/BoxListener.java
@@ -0,0 +1,21 @@
+package classes;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class BoxListener implements ActionListener {
+
+	private JComboBox<String> box;
+	private JTextArea area;
+	
+	BoxListener(JComboBox<String> box, JTextArea area) {
+		this.box = box;
+		this.area = area;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		int index = box.getSelectedIndex();
+		area.setText(CountryInformation.INFORMATION[index]);		
+	}
+
+}
---
+++ h04/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author Max
+ *
+ */
+module h04 {
+	requires java.desktop;
+}
